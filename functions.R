e11e74a777c752800bf53383d33a69ca348ccceb
Emanuele Spirito <emanuele.spirito.work@gmail.com>
2025-09-05 16:02:18 +0200

    250905 end
    new procedure. Users can now choose the sequence of analysis they want on their products and the software will recognize the files in the folder

---
+++ functions.R
@@ -0,0 +1,207 @@
+
+#_____________________________________________________________________
+#prismaread ----
+#_____________________________________________________________________
+prismaread_function <- function(product_type, he5_file){
+  
+  if(product_type == "L1"){
+    CLOUD <- T
+    ATCOR <- T
+  }
+  if(product_type == "L2"){
+    CLOUD <- F
+    ATCOR <- F
+  }
+  
+  prismaread::pr_convert(
+    ANGLES = T,
+    in_file = he5_file,
+    out_folder = paste0(base::dirname(he5_file),"/"),
+    out_format = "GTiff",
+    base_georef = T,
+    VNIR = F,
+    SWIR = F,
+    FULL = T,
+    fill_gaps = T,
+    source = "HCO",
+    join_priority = "SWIR",
+    LATLON = T,
+    PAN = F,
+    CLOUD = CLOUD,
+    overwrite = T,
+    ATCOR = ATCOR
+  )
+}
+
+#_____________________________________________________________________
+#atcor and prisma_angle ----
+#_____________________________________________________________________
+atcor_parameters <- function(angle_file_path){
+  prismaread_angle_file <- utils::read.table(angle_file_path, header =T)
+  PRISMA_angle_command <- base::paste0("python"," ",getwd(),"/PRISMA_angle.py")
+  sensor_angles <- base::system(PRISMA_angle_command, intern = TRUE) #per questo ci serve h5py in python
+  
+  sensor_zenith <- strsplit(sensor_angles,",")[[1]][1]
+  sensor_zenith <- gsub('^.', '', sensor_zenith)
+  
+  sensor_azimuth <- strsplit(sensor_angles,",")[[1]][2]
+  sensor_azimuth <- gsub('^.|.$', '', sensor_azimuth)
+  
+  prismaread_angle_file$sensor_zenith <- as.numeric(sensor_zenith)
+  prismaread_angle_file$sensor_azimuth <- as.numeric(sensor_azimuth)
+  
+  write.csv(prismaread_angle_file,paste0(base::dirname(angle_file_path),"/ATCOR/all_angles_file.csv"), quote = F, row.names = F)
+}
+
+cloud_mask <- function(cloud_path, full_path){
+  cloud <- terra::rast(cloud_path)
+  cloud <- terra::subst(cloud, NA, 1)
+  terra::plot(cloud, range = c(0,1))
+  
+  cloud_dil <- spatialist::erodil_raster(raster = cloud, 
+                                         width = c(3,3), 
+                                         type = "box", 
+                                         erosion = T, 
+                                         dilation = T,
+                                         erosion_first = T, 
+                                         nt = 1)
+  
+  terra::plot(cloud_dil)
+  
+  full <- terra::rast(full_path)
+  
+  terra::add(full) <- cloud_dil
+  
+  terra::writeRaster(full, 
+                     base::gsub("_HCO_FULL.tif","_HCO_FULL_CLD.tif",full_path),
+                     overwrite = T)
+}
+
+#_____________________________________________________________________
+#coreg ----
+#_____________________________________________________________________
+coregistration_to_s2 <- function(s2_file,coreg_input_path,coreg_proj_path,coreg_out_folder){
+  #create single layer image to coregister and change crs to EPSG:32632
+  target_epsg <- paste0("epsg:",terra::crs(terra::rast(s2_file),T,T,T)[3]$code)
+  
+  prisma_projected <- terra::project(x = terra::rast(coreg_input_path),
+                                     y = target_epsg,
+                                     method = "near")
+  terra::writeRaster(prisma_projected, 
+                     coreg_proj_path,
+                     overwrite = T)
+  prisma_b52 <- terra::subset(x = prisma_projected, subset = 52)
+  terra::writeRaster(prisma_b52, 
+                     base::gsub("proj","proj_52",coreg_proj_path),
+                     overwrite = T)
+  
+  # set arguments
+  single_band_reference_image <- s2_file
+  single_band_image_to_coregister <- base::list.files(base::dirname(s2_file),"\\proj_52.tif", full.names = T)
+  multiband_image_to_coregister <- base::list.files(base::dirname(s2_file),"\\proj.tif", full.names = T)
+  arosics_local_path <- base::paste0("python"," ",base::getwd(),"/arosics_local.py")
+  output_directory <- coreg_out_folder
+  output_file <- base::paste0(coreg_out_folder,"/prs_crs_translate_warp.tif")
+  
+  base::dir.create(coreg_out_folder, recursive = T, showWarnings = F)
+  
+  # setup AROSICS run command
+  #IMPROVE: potrei usare AROSICS da riga di comando?
+  arosics_run_command <- base::paste(arosics_local_path, "-v -r", single_band_reference_image, "-t", single_band_image_to_coregister, "-l", base::normalizePath(path=base::paste(output_directory, "/", "AROSICS_coregistration_info.json", sep=""), winslash="/", mustWork=FALSE), "-m", base::normalizePath(path=base::paste(output_directory, "/", "GCP.txt", sep=""), winslash="/", mustWork=FALSE), "-g", base::normalizePath(path=base::paste(output_directory, "/", "points.gpkg", sep=""), winslash="/", mustWork=FALSE), sep=" ")
+  # run AROSICS to get GCPs
+  base::system(arosics_run_command)
+  # import GCPs
+  gcp_lines <- base::readLines(con=base::normalizePath(path=base::paste(output_directory, "/", "GCP.txt", sep=""), winslash="/", mustWork=FALSE))
+  # generate GCP line to be used in GDAL
+  gcp_args <- base::paste(base::paste("-gcp", gcp_lines, sep=" "), collapse=" ")
+  # create VRT with GCP
+  GDAL_VRT_run_command <- base::paste("gdal_translate -q -of VRT", gcp_args, multiband_image_to_coregister, base::normalizePath(path=base::paste(output_directory, "/", "multiband_file_with_GCP.vrt", sep=""), winslash="/", mustWork=FALSE), sep=" ")
+  base::system(GDAL_VRT_run_command)
+  # warp input image using second order polynomial
+  GDAL_WARP_run_command <- base::paste("gdalwarp -q -of GTiff -r near -order 2 -tap -tr 30 30 -t_srs", target_epsg, base::normalizePath(path=base::paste(output_directory, "/", "multiband_file_with_GCP.vrt", sep=""), winslash="/", mustWork=TRUE), output_file, sep=" ")
+  base::system(GDAL_WARP_run_command)
+  
+}
+
+#_____________________________________________________________________
+#regrid ----
+#_____________________________________________________________________
+regrid_function <- function(master_image_path, name_of_current_output_folder, regrid_input_path){
+  terra::extend(x = terra::rast(master_image_path),
+                y = terra::rast(regrid_input_path),
+                fill = 0,
+                filename = base::paste0(name_of_current_output_folder,"/PRISMA_extend.tif"),
+                overwrite = T)
+  
+  terra::resample(x = terra::rast(regrid_input_path),
+                  y = terra::rast(base::paste0(name_of_current_output_folder,"/PRISMA_extend.tif")),
+                  method = resample_type,
+                  threads = T,
+                  by_util = T,
+                  filename = base::paste0(name_of_current_output_folder,"/PRISMA_resample.tif"),
+                  overwrite = T)
+  
+  file.remove(base::paste0(name_of_current_output_folder,"/PRISMA_extend.tif"))
+  
+}
+
+#_____________________________________________________________________
+#smooth ----
+#_____________________________________________________________________
+smooth_spectra <- function(terra_image_path,smoothing_out,cloud_smooth){
+  
+  PRISMA_config <- tidytable::fread(base::paste0(base::getwd(),"/PRISMA_spectral_configuration.csv")) %>%
+    tidytable::mutate(band_row = tidytable::row_number()) 
+  
+  PRISMA_bad_bands_table <- tidytable::fread(base::paste0(base::getwd(),"/PRISMA_band_selections.csv")) %>%
+    tidytable::filter(BB_SUPER_V3 == 1)
+  
+  input_bad_bands <- PRISMA_bad_bands_table$band
+  
+  input_wvl <- PRISMA_config$center
+  
+  output_wvl <- PRISMA_config %>%
+    tidytable::filter(BND_SEL  == 1) %>%
+    tidytable::pull(center)
+  
+  
+  spline_fun <- function(pixel, band_center_input, bad_bands_pos, band_center_output, df = 40) {
+    # togliamo le bande cattive
+    ref_valid <- pixel[-bad_bands_pos]
+    wvl_valid <- band_center_input[-bad_bands_pos]
+    
+    if (base::any(is.na(ref_valid))) {
+      return(base::rep(NA_real_, base::length(band_center_output)))
+    }
+    
+    sp <- stats::smooth.spline(x = wvl_valid, y = ref_valid, df = df)
+    y_smooth <- stats::predict(sp, x = band_center_output)$y
+    # limitiamo a zero
+    y_smooth[y_smooth < 0] <- 0
+    return(y_smooth)
+  }
+  
+  
+  terra_image <- terra::rast(terra_image_path)
+  
+  if(cloud_smooth){
+    terra_image_sub <- terra::subset(terra_image,subset = 231,negate=T)
+  }else{
+    terra_image_sub <- terra_image
+  }
+  
+  output_image <- terra::app(
+    x = terra_image_sub,
+    fun = spline_fun,
+    band_center_input = input_wvl, 
+    bad_bands_pos = input_bad_bands, 
+    band_center_output = output_wvl,
+    df=40,
+    
+    cores = 7,                     
+    filename = smoothing_out,
+    overwrite = TRUE,
+    wopt = base::list(gdal = c("COMPRESS=LZW", "TILED=YES"))
+  )
+  
+}
--- main.R
+++ main.R
@@ -29,272 +29,151 @@
 #inputs ----
 #_____________________________________________________________________
 #for normal users: modify only things in this section
-regridding_option <- T
-smoothing_option <- T
+regrid_option <- "N" #can be N for near, B for bilinear, C for cubic
+
+#for expert users:
+procedure_order <- c("read","cloud","coreg","atcor","regrid","smooth")
+#elements: read, atcor, cloud, coreg, regrid, smooth
 
 #_____________________________________________________________________
 #setup folders ----
 #_____________________________________________________________________
+source("/space/functions.R")
 out_folder  <-  "/space/put_PRISMA_he5_and_S2_tif_here/"
-in_file <- base::list.files(path = out_folder, pattern = "\\.he5$", ignore.case = T, full.names = T)
-s2_file <- base::list.files(path = out_folder, pattern = "\\.tif$", ignore.case = T, full.names = T)
+s2_folder <- out_folder
 base::setwd(base::dirname(out_folder))
-product_type <- base::substring(base::basename(in_file),5,6)
-coreg_out_folder <- base::paste0(out_folder,"1_coreg/")
-regrid_out_folder <- base::paste0(out_folder,"2_regrid/")
-smoothing_out_folder <- base::paste0(out_folder,"3_smoothing/")
+
+he5_file <- base::list.files(path = out_folder, pattern = "\\.he5$", ignore.case = T, full.names = T)
+product_type <- base::substring(base::basename(he5_file),5,6)
 
 #_____________________________________________________________________
-#prismaread ----
+#work ----
 #_____________________________________________________________________
-if(product_type == "L1"){
-  CLOUD <- T
-  ATCOR <- T
-}
-if(product_type == "L2"){
-  CLOUD <- F
-  ATCOR <- F
-}
-
-prismaread::pr_convert(
-  ANGLES = T,
-  in_file = in_file,
-  out_folder = out_folder,
-  out_format = "GTiff",
-  base_georef = T,
-  VNIR = F,
-  SWIR = F,
-  FULL = T,
-  fill_gaps = T,
-  source = "HCO",
-  join_priority = "SWIR",
-  LATLON = T,
-  PAN = F,
-  CLOUD = CLOUD,
-  overwrite = T,
-  ATCOR = ATCOR
-)
-
-#_____________________________________________________________________
-#write ATCOR parameters ----
-#_____________________________________________________________________
-if(product_type == "L1"){
-  prismaread_angle_file_path <- base::paste0(out_folder,base::gsub(".he5","_HCO.ang",base::basename(in_file)))
-  prismaread_angle_file <- utils::read.table(prismaread_angle_file_path, header =T)
-  PRISMA_angle_command <- base::paste0("python"," ",getwd(),"/PRISMA_angle.py")
-  sensor_angles <- base::system(PRISMA_angle_command, intern = TRUE) #per questo ci serve h5py in python
+number_of_operations <- length(procedure_order)
+name_of_current_output_folder <- ""
+for(index_of_operations in 1:number_of_operations){
+  current_operation <- procedure_order[index_of_operations]
   
-  sensor_zenith <- strsplit(sensor_angles,",")[[1]][1]
-  sensor_zenith <- gsub('^.', '', sensor_zenith)
-  
-  sensor_azimuth <- strsplit(sensor_angles,",")[[1]][2]
-  sensor_azimuth <- gsub('^.|.$', '', sensor_azimuth)
-  
-  prismaread_angle_file$sensor_zenith <- as.numeric(sensor_zenith)
-  prismaread_angle_file$sensor_azimuth <- as.numeric(sensor_azimuth)
-  
-  write.csv(prismaread_angle_file,paste0(out_folder,"ATCOR/all_angles_file.csv"), quote = F, row.names = F)
-}
-
-#_____________________________________________________________________
-#preparing ----
-#_____________________________________________________________________
-#work on cloud mask for pixel strips
-if(product_type == "L1"){
-  cloud <- terra::rast(base::paste0(out_folder,base::gsub(".he5","_HCO_CLD.tif",base::basename(in_file))))
-  cloud <- terra::subst(cloud, NA, 1)
-  terra::plot(cloud, range = c(0,1))
-  
-  full <- terra::rast(base::paste0(out_folder,base::gsub(".he5","_HCO_FULL.tif",base::basename(in_file))))
-  
-  cloud_dil <- spatialist::erodil_raster(raster = cloud, 
-                                         width = c(3,3), 
-                                         type = "box", 
-                                         erosion = T, 
-                                         dilation = T,
-                                         erosion_first = T, 
-                                         nt = 1)
-  
-  terra::plot(cloud_dil)
-  
-  terra::add(full) <- cloud_dil
-  
-  terra::writeRaster(full, base::paste0(out_folder,base::gsub(".he5","_HCO_FULL_CLD.tif",base::basename(in_file))),
-                     overwrite = T)
-  
-  prisma_input <- base::paste0(out_folder,base::gsub(".he5","_HCO_FULL_CLD.tif",base::basename(in_file)))
-}
-if(product_type == "L2"){
-  prisma_input <- base::paste0(out_folder,base::gsub(".he5","_HCO_FULL.tif",base::basename(in_file)))
-}
-
-#create single layer image to coregister and change crs to EPSG:32632
-prisma_projected <- terra::project(x = terra::rast(prisma_input),
-                                       y = "epsg:32632",
-                                       method = "near")
-terra::writeRaster(prisma_projected, 
-                   base::paste0(out_folder,base::gsub(".he5","_HCO_FULL_proj.tif",base::basename(in_file))),
-                   overwrite = T)
-prisma_b52 <- terra::subset(x = prisma_projected, subset = 52)
-terra::writeRaster(prisma_b52, 
-                   base::paste0(out_folder,base::gsub(".he5","_HCO_FULL_proj_52.tif",base::basename(in_file))),
-                   overwrite = T)
-
-#_____________________________________________________________________
-#coregistration ----
-#_____________________________________________________________________
-# set arguments
-single_band_reference_image <- s2_file
-single_band_image_to_coregister <- base::paste0(out_folder,base::gsub(".he5","_HCO_FULL_proj_52.tif",base::basename(in_file)))
-multiband_image_to_coregister <- base::paste0(out_folder,base::gsub(".he5","_HCO_FULL_proj.tif",base::basename(in_file)))
-arosics_local_path <- base::paste0("python"," ",base::getwd(),"/arosics_local.py")
-target_epsg <- "'EPSG:32632'"
-output_directory <- coreg_out_folder
-output_file <- base::paste0(coreg_out_folder,"prs_crs_translate_warp.tif")
-
-base::dir.create(coreg_out_folder, recursive = T, showWarnings = F)
-
-# setup AROSICS run command
-#IMPROVE: potrei usare AROSICS da riga di comando?
-arosics_run_command <- base::paste(arosics_local_path, "-v -r", single_band_reference_image, "-t", single_band_image_to_coregister, "-l", base::normalizePath(path=base::paste(output_directory, "/", "AROSICS_coregistration_info.json", sep=""), winslash="/", mustWork=FALSE), "-m", base::normalizePath(path=base::paste(output_directory, "/", "GCP.txt", sep=""), winslash="/", mustWork=FALSE), "-g", base::normalizePath(path=base::paste(output_directory, "/", "points.gpkg", sep=""), winslash="/", mustWork=FALSE), sep=" ")
-# run AROSICS to get GCPs
-base::system(arosics_run_command)
-# import GCPs
-gcp_lines <- base::readLines(con=base::normalizePath(path=base::paste(output_directory, "/", "GCP.txt", sep=""), winslash="/", mustWork=FALSE))
-# generate GCP line to be used in GDAL
-gcp_args <- base::paste(base::paste("-gcp", gcp_lines, sep=" "), collapse=" ")
-# create VRT with GCP
-GDAL_VRT_run_command <- base::paste("gdal_translate -q -of VRT", gcp_args, multiband_image_to_coregister, base::normalizePath(path=base::paste(output_directory, "/", "multiband_file_with_GCP.vrt", sep=""), winslash="/", mustWork=FALSE), sep=" ")
-base::system(GDAL_VRT_run_command)
-# warp input image using second order polynomial
-target_epsg <- "'EPSG:32632'"
-GDAL_WARP_run_command <- base::paste("gdalwarp -q -of GTiff -r near -order 2 -tap -tr 30 30 -t_srs", target_epsg, base::normalizePath(path=base::paste(output_directory, "/", "multiband_file_with_GCP.vrt", sep=""), winslash="/", mustWork=TRUE), output_file, sep=" ")
-base::system(GDAL_WARP_run_command)
-
-#_____________________________________________________________________
-#regrid ----
-#_____________________________________________________________________
-#IMPROVE: should I use gdalUtils::gdalwarp or sen2r::gdalwarp_grid??
-if(regridding_option){
-  base::dir.create(regrid_out_folder, recursive = T, showWarnings = F)
-  
-  if(product_type == "L1"){
-    resample_type <- "near"
+  #procedures that are the start of a sequence
+  if(current_operation == "read"){
+    print("READ")
+    if(identical(he5_file,character(0))){
+      print("ERRORE")
+    }else{
+      prismaread_function(product_type, he5_file)
+    }
   }
-  if(product_type == "L2"){
-    resample_type <- "cubicspline"
+  if(current_operation == "cloud"){
+    print("CLOUD")
+    cloud_path <- base::list.files(path = out_folder, pattern = "\\_HCO_CLD.tif", full.names = T)
+    full_path <- base::list.files(path = out_folder, pattern = "\\_HCO_FULL.tif", full.names = T)
+    if(identical(cloud_path,character(0)) | identical(full_path,character(0))){
+      print("ERRORE")
+    }else{
+      cloud_mask(cloud_path, full_path)
+    }
+  }
+  if(current_operation == "atcor"){
+    print("ATCOR")
+    angle_file_path <- base::list.files(path = out_folder, pattern = "\\HCO.ang", full.names = T)
+    if(identical(he5_file,character(0)) | identical(angle_file_path,character(0))){
+      print("ERRORE")
+    }else{
+      atcor_parameters(angle_file_path)
+      #when atcor will be implemented in this procedure, here you will find and atcor will be in 
+      #the next operations category 
+    }
   }
   
-  #NOTE: for Jolanda di Savoia use master image 
-  #from //10.0.1.243/projects/2022_ASI-PRIS4VEG/3-DATA/images/PRISMA_img_master/PRS_L2D_STD_20200407_HCO_JDS_EXT_FULL_30m_smooth_v1_170b
-  #for Piacenza the master image is not available yet
-  
-  master_image_dir <- base::paste0(base::getwd(),"/master_image_for_regridding/")
-  
-  master_image_path <- base::list.files(master_image_dir, full.names = T, pattern = "\\.tif$")
-  
-  coreg_out <- base::paste0(coreg_out_folder,"prs_crs_translate_warp.tif")
-  #coreg_out <- base::paste0(coreg_out_folder,"PRS_L1_STD_OFFL_20231004_geocoded_ENVI_atm.bsq")
-  
-  terra::extend(x = terra::rast(master_image_path),
-                y = terra::rast(coreg_out),
-                fill = 0,
-                filename = base::paste0(regrid_out_folder,"PRISMA_extend.tif"),
-                overwrite = T)
-  
-  terra::resample(x = terra::rast(coreg_out),
-                  y = terra::rast(base::paste0(regrid_out_folder,"PRISMA_extend.tif")),
-                  method = resample_type,
-                  threads = T,
-                  by_util = T,
-                  filename = base::paste0(regrid_out_folder,"PRISMA_resample.tif"),
-                  overwrite = T)
-  
-  input_image_path <- base::paste0(regrid_out_folder,"PRISMA_resample.tif")
-}else{
-  input_image_path <- base::paste0(coreg_out_folder,"prs_crs_translate_warp.tif")
-}
-
-
-#_____________________________________________________________________
-#atcor ----
-#_____________________________________________________________________
-if(product_type == "L1"){
-  print("here we need to put atcor")
-  
-}
-
-#_____________________________________________________________________
-#smoothing ----
-#_____________________________________________________________________
-if(smoothing_option){
-  if(product_type == "L2"){
-    library(tidytable)
+  #procedures that can be swapt
+  if(current_operation == "coreg"){
+    #chain part
+    if(name_of_current_output_folder == ""){
+      name_of_current_output_folder <- paste0(out_folder,current_operation)
+    }else{
+      out_folder <- name_of_current_output_folder
+      name_of_current_output_folder <- paste0(name_of_current_output_folder,"_",current_operation)
+    }
     
-    base::dir.create(smoothing_out_folder, recursive = T, showWarnings = F)
+    base::dir.create(name_of_current_output_folder, recursive = T, showWarnings = F)
     
-    smoothing_out <-  base::paste0(smoothing_out_folder, "PRISMA_smoothed.tif")
-    
-    PRISMA_config <- tidytable::fread(base::paste0(base::getwd(),"/PRISMA_spectral_configuration.csv")) %>%
-      tidytable::mutate(band_row = tidytable::row_number()) 
-    
-    PRISMA_bad_bands_table <- tidytable::fread(base::paste0(base::getwd(),"/PRISMA_band_selections.csv")) %>%
-      tidytable::filter(BB_SUPER_V3 == 1)
-    
-    input_bad_bands <- PRISMA_bad_bands_table$band
-    
-    input_wvl <- PRISMA_config$center
-    
-    output_wvl <- PRISMA_config %>%
-      tidytable::filter(BND_SEL  == 1) %>%
-      tidytable::pull(center)
-    
-    
-    spline_fun <- function(pixel, band_center_input, bad_bands_pos, band_center_output, df = 40) {
-      # togliamo le bande cattive
-      ref_valid <- pixel[-bad_bands_pos]
-      wvl_valid <- band_center_input[-bad_bands_pos]
-      
-      if (base::any(is.na(ref_valid))) {
-        return(base::rep(NA_real_, base::length(band_center_output)))
+    #coregistration part
+    s2_file <- base::list.files(path = s2_folder, pattern = glob2rx("S2*.tif"), ignore.case = T, full.names = T)
+    coreg_input_path <- base::list.files(path = out_folder, pattern = "\\HCO_FULL_CLD.tif", full.names = T)
+    coreg_proj_path <- base::gsub("FULL_CLD","FULL_CLD_proj",coreg_input_path)
+    if(identical(coreg_input_path,character(0))){
+      coreg_input_path <- base::list.files(path = out_folder, pattern = "\\HCO_FULL.tif", full.names = T)
+      coreg_proj_path <- base::gsub("FULL","FULL_proj",coreg_input_path)
+      if(identical(coreg_input_path,character(0))){
+        coreg_input_path <- base::list.files(path = out_folder, pattern = glob2rx("*.tif"), ignore.case = T, full.names = T)
+        coreg_input_path <- coreg_input_path[!substr(basename(coreg_input_path),0,2) == "S2"]
+        coreg_proj_path <- gsub(".tif","_proj.tif",coreg_input_path)
       }
-      
-      sp <- stats::smooth.spline(x = wvl_valid, y = ref_valid, df = df)
-      y_smooth <- stats::predict(sp, x = band_center_output)$y
-      # limitiamo a zero
-      y_smooth[y_smooth < 0] <- 0
-      return(y_smooth)
     }
     
-    
-    terra_image <- terra::rast(input_image_path)
-    
-    if(product_type == "L1"){
-      terra_image_sub <- terra::subset(terra_image,subset = 231,negate=T)
-    }
-    if(product_type == "L2"){
-      terra_image_sub <- terra_image
-    }
-    
-    output_image <- terra::app(
-      x = terra_image_sub,
-      fun = spline_fun,
-      band_center_input = input_wvl, 
-      bad_bands_pos = input_bad_bands, 
-      band_center_output = output_wvl,
-      df=40,
-      
-      cores = 7,                     
-      filename = smoothing_out,
-      overwrite = TRUE,
-      wopt = base::list(gdal = c("COMPRESS=LZW", "TILED=YES"))
-    )
-    
+    coregistration_to_s2(s2_file,coreg_input_path,coreg_proj_path,name_of_current_output_folder)
   }
-  if(product_type == "L1"){
-    print("come here when you are done with ATCOR")
+  if(current_operation == "regrid"){
+    #chain part
+    if(name_of_current_output_folder == ""){
+      name_of_current_output_folder <- paste0(out_folder,current_operation)
+    }else{
+      out_folder <- name_of_current_output_folder
+      name_of_current_output_folder <- paste0(name_of_current_output_folder,"_",current_operation)
+    }
+    
+    base::dir.create(name_of_current_output_folder, recursive = T, showWarnings = F)
+    
+    #regrid part
+    
+    if(regrid_option == "C"){
+      resample_type <- "cubicspline"
+    }else if(regrid_option == "B"){
+      resample_type <- "bilinear"
+    }else{
+      resample_type <- "near"
+    }
+    
+    #NOTE: for Jolanda di Savoia use master image 
+    #from //10.0.1.243/projects/2022_ASI-PRIS4VEG/3-DATA/images/PRISMA_img_master/PRS_L2D_STD_20200407_HCO_JDS_EXT_FULL_30m_smooth_v1_170b
+    #for Piacenza the master image is not available yet
+    
+    master_image_path <- base::list.files(base::paste0(base::getwd(),"/master_image_for_regridding/"), full.names = T, pattern = "\\.tif$")
+    
+    #coreg_out <- base::paste0(coreg_out_folder,"prs_crs_translate_warp.tif")
+    #coreg_out <- base::paste0(coreg_out_folder,"PRS_L1_STD_OFFL_20220401_geocoded_ENVI_atm.bsq")
+    
+    regrid_input_path <- base::list.files(out_folder, full.names = T, pattern = "\\.tif")
+    
+    regrid_function(master_image_path, name_of_current_output_folder, regrid_input_path)
   }
+  if(current_operation == "smooth"){
+    #chain part
+    if(name_of_current_output_folder == ""){
+      name_of_current_output_folder <- paste0(out_folder,current_operation)
+    }else{
+      out_folder <- name_of_current_output_folder
+      name_of_current_output_folder <- paste0(name_of_current_output_folder,"_",current_operation)
+    }
+    
+    base::dir.create(name_of_current_output_folder, recursive = T, showWarnings = F)
+    
+    #smooth part
+    smoothing_out <-  base::paste0(name_of_current_output_folder, "/PRISMA_smoothed.tif")
+    
+    if("cloud" %in% procedure_order){
+      cloud_smooth <- T
+    }else{
+      cloud_smooth <- F
+    }
+    
+    terra_image_path <- base::list.files(out_folder, pattern = "\\.tif", full.names = T)
+    
+    smooth_spectra(terra_image_path,smoothing_out,cloud_smooth)
+  }
+  
 }
+
 
 
 
